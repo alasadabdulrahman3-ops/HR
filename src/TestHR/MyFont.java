d8f186594ca1c0b1764074bef6a74388e2f22d30
unknown <youremail@yourdomain.com>
2026-01-02 20:06:55 +0300

    update

--- src/TestHR/MyFont.java
+++ src/TestHR/MyFont.java
@@ -2,6 +2,7 @@ package TestHR;
 import java.io.File;
 import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -15,21 +16,22 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class MyFont extends MYData{
-
-    WebDriver driver;
+public class MyFont   {
+	
+   WebDriver driver;
     String URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
 String ADD = "https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmployee";
+
     @BeforeTest
     public void setup() {
         driver = new ChromeDriver();
-        driver.get(URL);
+       driver.get(URL);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
     @Test(priority = 1)
-    public void login() {
+    public void login() throws InterruptedException{
 
         WebElement Username = driver.findElement(By.name("username"));
         WebElement Password = driver.findElement(By.name("password"));
@@ -37,56 +39,75 @@ String ADD = "https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmp
 
         Username.sendKeys("Admin");
         Password.sendKeys("admin123");
+        Thread.sleep(1000);
         loginBtn.click();
     }
     @Test (priority = 2)
-    public void PIM () {
-    	WebElement Pim = driver.findElement(
-    		    By.xpath("//span[text()='PIM']")
-    		);
-    		Pim.click();
-
-    }
+    public void PIM () {WebElement Pim = driver.findElement( By.xpath("//span[text()='PIM']"));
+    		Pim.click();   }
     
-     @Test (priority =4)
+     @Test (priority =3)
      public void AddEmployee() {
-      driver.get(ADD);
-      WebElement EmployeeFullName = driver.findElement(
-    		    By.cssSelector(".oxd-input.oxd-input--active.orangehrm-firstname")
-    		);
-      EmployeeFullName.sendKeys("Abdul Rahman ");
-      WebElement MiddleName = driver.findElement(
-    		    By.cssSelector(".oxd-input.oxd-input--active.orangehrm-middlename")
-    		);
-    		MiddleName.sendKeys("Ahmad");
 
-    		WebElement lastName = driver.findElement(
-    			    By.name("lastName")
-    			);
-    			lastName.sendKeys("Al-Asaad");
+         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-    			WebElement employeeId = driver.findElement(
-    				    By.xpath("//label[text()='Employee Id']/following::input[1]")
-    				);
-    			employeeId.sendKeys(Keys.chord(Keys.CONTROL, "a")); // تحديد كل النص
-    				employeeId.sendKeys(Keys.DELETE); 
-    				employeeId.sendKeys(Keys.chord(Keys.CONTROL, "a"));
-    				employeeId.sendKeys(Keys.DELETE);
-    				employeeId.sendKeys("86");
-    				WebElement Save = driver.findElement(
-    					    By.cssSelector(".oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space")
-    					);
-    				
-    					Save.click();}
-    	/////////////////			
-     @SuppressWarnings("unused")
-	@Test (priority =5, enabled = false )
+         driver.get(ADD);
+
+         // 🔹 بيانات عشوائية
+         String firstName = generateRandomName("FN");
+         String middleName = generateRandomName("MN");
+         String lastName = generateRandomName("LN");
+         String empId = generateRandomNumber(4);
+
+         // 🔹 First Name
+         WebElement firstNameField = wait.until(
+                 ExpectedConditions.visibilityOfElementLocated(
+                         By.cssSelector(".oxd-input.oxd-input--active.orangehrm-firstname")
+                 )
+         );
+         firstNameField.sendKeys(firstName);
+
+         // 🔹 Middle Name
+         WebElement middleNameField = driver.findElement(
+                 By.cssSelector(".oxd-input.oxd-input--active.orangehrm-middlename")
+         );
+         middleNameField.sendKeys(middleName);
+
+         // 🔹 Last Name
+         WebElement lastNameField = driver.findElement(By.name("lastName"));
+         lastNameField.sendKeys(lastName);
+
+         // 🔹 Employee ID
+         WebElement employeeIdField = driver.findElement(
+                 By.xpath("//label[text()='Employee Id']/following::input[1]")
+         );
+         employeeIdField.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+         employeeIdField.sendKeys(Keys.DELETE);
+         employeeIdField.sendKeys(empId);
+
+         // 🔹 Save
+         WebElement saveBtn = wait.until(  ExpectedConditions.elementToBeClickable( By.cssSelector(".oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space"))
+         );
+         saveBtn.click();
+     }
+
+     public String generateRandomName(String prefix) {
+         Random random = new Random();
+         return prefix + random.nextInt(1000);
+     }
+     public String generateRandomNumber(int length) {
+         Random random = new Random();
+         StringBuilder number = new StringBuilder();
+         for (int i = 0; i < length; i++) {
+             number.append(random.nextInt(10));
+         }
+         return number.toString(); }
+
+     	@Test (priority =4, enabled = false )
      public void Information() {
 
     	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     	    JavascriptExecutor js = (JavascriptExecutor) driver;
-    	    Actions actions = new Actions(driver);
-
     	    // =============== Other Id ===============
     	    WebElement otherId = wait.until(ExpectedConditions.elementToBeClickable(
     	            By.xpath("//label[text()='Other Id']/following::input[1]")));
@@ -145,7 +166,7 @@ String ADD = "https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmp
 
     	}
 
-     @Test (priority =6 , enabled = false)
+     @Test (priority =5 , enabled = false)
      public void CustomFields() {
 
     	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
@@ -176,10 +197,7 @@ String ADD = "https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmp
     	    actions.moveToElement(secondSaveButton).click().perform();
     	}
      
-     
-     
-
-     @Test (priority =7)
+     @Test (priority =6, enabled = false)
      public void uploadAttachment(WebDriver driver) throws InterruptedException {
     	    // زيادة مدة الانتظار لتجنب مشاكل العناصر البطيئة
     	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
@@ -219,11 +237,86 @@ String ADD = "https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmp
     	    saveBtn.click();
     	    System.out.println("✅ تم حفظ الملف بنجاح!");
     	}
-	 
-    	
-     
-     }
+	 @Test (priority = 7)
+	 public void ContactDetails() {
+		 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		 WebElement contactTab = wait.until(
+		     ExpectedConditions.elementToBeClickable(By.linkText("Contact Details"))
+		 );
+		 contactTab.click();}
+		
+@Test (priority = 8)
+public static void fillAddress(WebDriver driver) {
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    Random random = new Random();
 
+    // 🔹 بيانات واقعية
+    String[] streets = {
+        "King Abdullah Street", "Rainbow Street", "Al-Wehdat Street",
+        "Al-Madina Street", "Al-Quds Street", "Al-Hussein Street"
+    };
 
+    String[] cities = {
+        "Amman", "Zarqa", "Irbid", "Aqaba", "Madaba", "Salt", "Jerash"
+    };
+
+    String[] states = {
+        "Amman", "Balqa", "Irbid", "Aqaba", "Mafraq", "Karak", "Tafilah"
+    };
+
+    // 🔹 Street 1
+    WebElement street1 = wait.until(ExpectedConditions.elementToBeClickable(
+        By.xpath("//label[text()='Street 1']/../following-sibling::div//input")
+    ));
+    street1.clear();
+    street1.sendKeys(streets[random.nextInt(streets.length)]);
+
+    // 🔹 Street 2
+    WebElement street2 = wait.until(ExpectedConditions.elementToBeClickable(
+        By.xpath("(//label[text()='Street 2']/../following-sibling::div//input)[2]")
+    ));
+    street2.clear();
+    street2.sendKeys("Apt " + (random.nextInt(500) + 1));
+
+    // 🔹 City
+    WebElement city = wait.until(ExpectedConditions.elementToBeClickable(
+        By.xpath("//label[text()='City']/../following-sibling::div//input")
+    ));
+    city.clear();
+    city.sendKeys(cities[random.nextInt(cities.length)]);
+
+    // 🔹 State/Province
+    WebElement state = wait.until(ExpectedConditions.elementToBeClickable(
+        By.xpath("//label[text()='State/Province']/../following-sibling::div//input")
+    ));
+    state.clear();
+    state.sendKeys(states[random.nextInt(states.length)]);
+
+    // 🔹 Zip/Postal Code
+    WebElement zip = wait.until(ExpectedConditions.elementToBeClickable(
+        By.xpath("//label[text()='Zip/Postal Code']/../following-sibling::div//input")
+    ));
+    zip.clear();
+    zip.sendKeys(String.format("%05d", random.nextInt(99999)));
+
+    // 🔹 Country dropdown
+    WebElement countryDropdown = wait.until(ExpectedConditions.elementToBeClickable(
+        By.xpath("//label[text()='Country']/../following-sibling::div//div[contains(@class,'oxd-select-text')]")
+    ));
+    countryDropdown.click();
+
+    // ⚡ انتظر ظهور العناصر
+    List<WebElement> countries = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+        By.xpath("//div[@role='option']")
+    ));
+
+    // ⚡ scroll واختيار عنصر عشوائي
+    WebElement randomCountry = countries.get(random.nextInt(countries.size()));
+    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", randomCountry);
+    randomCountry.click();
+
+    System.out.println("✅ Contact Details form filled successfully!");
+}
+}
     
 
